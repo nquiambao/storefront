f382d96ca2ec3036b3da450e04ad6e417cce5609
Nica Quiambao <n.quiambao@hotmail.com>
2022-04-20 16:54:09 -0600

    changed http-equiv

--- pages/index.js
+++ pages/index.js
@@ -32,7 +32,7 @@ export default function Home(props) {
     <>
     <Head>
       <meta charset="UTF-8"/>
-      <meta http-equiv="X-UA-Compatible" content="IE=edge"/>
+      <meta httpEquiv="X-UA-Compatible" content="IE=edge"/>
       <meta name="viewport" content="width=device-width, initial-scale=1.0"/>
       <meta name="description" content="speck eyewear provides all kinds of glasses for everyone."/>
       <title>Storefront</title>
